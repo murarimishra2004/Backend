ba67a4e0cba5abe6f0dd74d5d2003b75d164f11e
Murari Mishra <muraripcmb.1@gmail.com>
2024-07-14 12:38:36 +0530

    Part1

---
+++ Part1/scripts.js
@@ -0,0 +1,29 @@
+//fundamentals of js
+// arrays and objects
+// function return
+// async js coding
+// for each , map , filter , indexOf
+
+let arr=[1,2,3,4,5];
+// arr.forEach(function(val){
+//     console.log(val+"hello")
+
+// })
+
+// var ans= arr.map((v)=>{
+//     return(
+//         v+2
+//     )
+// })
+
+// let fil=arr.filter((val)=>{
+//     return val>3
+// })
+
+
+
+// let ans2=arr.find((val)=>{
+//     return val===2
+// })
+
+console.log(arr.indexOf(2))
